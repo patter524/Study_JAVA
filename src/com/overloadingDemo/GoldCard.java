8220027a70796be480b7041979aa8c23eeb16bab
patter008 <nitinai524@gmail.com>
2018-02-18 21:05:17 +0700

    gg

--- src/com/overloadingDemo/GoldCard.java
+++ src/com/overloadingDemo/GoldCard.java
@@ -1,5 +1,5 @@
 package com.overloadingDemo;
 
-public class Gold {
+public class GoldCard {
 	//annotation คือ ส่วนหนึ่งของโค้ดที่บอกรายละเอียดบางอย่าง   อย่างเช่นบอกให้รู้ว่า  PlatinumCard ทำการ Override method ที่อยู่ใน Class MemberCard
 }
